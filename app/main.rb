38ca9cbdd428ccffa455023ffa06091b03423ea7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-06-22 15:37:37 +0800

    Add: detail output for data_new & data_old

--- app/main.rb
+++ app/main.rb
@@ -40,23 +40,45 @@ def scraper
     fetched_at:      DateTime.now.localtime
   )
 
-  data_local = CovidKemkesPasien.all.last
+  data_local_last          = CovidKemkesPasien.all.last
+  data_local_before_last   = CovidKemkesPasien.all[-2]
+  data_new_positiv_covid   = data_input.positif_covid - data_local_last.positif_covid
+  data_new_sembuh_covid    = data_input.sembuh_covid - data_local_last.sembuh_covid
+  data_new_meninggal_covid = data_input.meninggal_covid - data_local_last.meninggal_covid
+  data_new_jumlah_odp      = data_input.jumlah_odp - data_local_last.jumlah_odp
+  data_new_jumlah_pdp      = data_input.jumlah_pdp - data_local_last.jumlah_pdp
+  data_old_positif_covid   = data_local_last.positif_covid - data_local_before_last.positif_covid
+  data_old_sembuh_covid    = data_local_last.sembuh_covid - data_local_before_last.sembuh_covid
+  data_old_meninggal_covid = data_local_last.meninggal_covid - data_local_before_last.meninggal_covid
+  data_old_jumlah_odp      = data_local_last.jumlah_odp - data_local_before_last.jumlah_odp
+  data_old_jumlah_pdp      = data_local_last.jumlah_pdp - data_local_before_last.jumlah_pdp
   # byebug
 
   if data_input.valid?
-    if (data_input.fetched_at.localtime.to_date != data_local.fetched_at.localtime.to_date) &&
-           (data_input.positif_covid != data_local.positif_covid)
+    if (data_input.fetched_at.localtime.to_date != data_local_last.fetched_at.localtime.to_date) &&
+           (data_input.positif_covid != data_local_last.positif_covid)
       data_input.save
-      puts "INFO: Data berhasil diinputkan ke dalam database!".bold.black.bg_brown
+      puts "INFO: DATA BERHASIL DIINPUTKAN KE DALAM DATABASE!".bold.black.bg_brown
       puts "Total Pasien Positif (REMOTE): " + "#{data_input.positif_covid}".bold
-      puts "Total Pasien Positif (LOCAL) : " + "#{data_local.positif_covid}".bold
-      puts "Total Pasien Positif Baru    : " + "#{data_input.positif_covid - data_local.positif_covid}".bold
+      puts "Total Pasien Positif (LOCAL) : " + "#{data_local_last.positif_covid}".bold
+      puts "DATA HARI INI".bold
+      puts "Total Pasien Positif Baru    : " + "#{data_new_positiv_covid}".bold
+      puts "Total Pasien Sembuh Baru     : " + "#{data_new_sembuh_covid}".bold
+      puts "Total Pasien Meninggal Baru  : " + "#{data_new_meninggal_covid}".bold
+      puts "Total ODP                    : " + "#{data_new_jumlah_odp}".bold
+      puts "Total PDP                    : " + "#{data_new_jumlah_pdp}".bold
     else
-      puts "INFO: Belum ada data baru untuk hari ini!".bold.black.bg_brown
+      puts "INFO: BELUM ADA DATA BARU UNTUK HARI INI!".bold.black.bg_brown
       puts "Total Pasien Positif (REMOTE): " + "#{data_input.positif_covid}".bold \
                                              + " (#{data_input.fetched_at.localtime.to_date})"
-      puts "Total Pasien Positif (LOCAL) : " + "#{data_local.positif_covid}".bold \
-                                             + " (#{data_local.fetched_at.localtime.to_date})"
+      puts "Total Pasien Positif (LOCAL) : " + "#{data_local_last.positif_covid}".bold \
+                                             + " (#{data_local_last.fetched_at.localtime.to_date})"
+      puts "SELISIH DATA HARI INI & KEMARIN".bold
+      puts "Total Pasien Positif Baru    : " + "#{data_old_positif_covid}".bold
+      puts "Total Pasien Sembuh Baru     : " + "#{data_old_sembuh_covid}".bold
+      puts "Total Pasien Meninggal Baru  : " + "#{data_old_meninggal_covid}".bold
+      puts "Total ODP                    : " + "#{data_old_jumlah_odp}".bold
+      puts "Total PDP                    : " + "#{data_old_jumlah_pdp}".bold
     end
   else
     puts "INFO: Data tidak valid!".bold.black.bg_red
